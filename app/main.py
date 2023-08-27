07dabe6021eaf292fcae0ba06f58414b1ad70506
heko <91366057+hkohko@users.noreply.github.com>
2023-08-27 13:29:07 +0700

    moved collage image to separate folder

--- app/main.py
+++ app/main.py
@@ -4,20 +4,23 @@ from os import getcwd, mkdir, listdir
 from os.path import exists
 
 MAIN_DIR = PurePath(__file__).parents[1]
+COLLAGE = MAIN_DIR.joinpath("collage")
 EXTRACTED_IMAGE = MAIN_DIR.joinpath("extracted_image")
 
 if not exists(str(EXTRACTED_IMAGE)):
     mkdir(str(EXTRACTED_IMAGE))
+if not exists(str(COLLAGE)):
+    mkdir(str(COLLAGE))
 
 
 def blur(img):
     img_rgb = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    blur_img = cv.GaussianBlur(img_rgb, tuple(31 for _ in range(2)), 8)
+    blur_img = cv.GaussianBlur(img_rgb, tuple(37 for _ in range(2)), 8)
     return blur_img
 
 
 def mask(blur_img):
-    low = 95
+    low = 105
     max_num = 255
     _, thresh = cv.threshold(blur_img, low, max_num, cv.THRESH_BINARY)
     return thresh
@@ -41,16 +44,16 @@ def show_boundingrect(cont, cont_image):
     cv.waitKey()
 
 
-def save_images(cont, image_subfolder):
-    idx = [int(x[:-4]) for x in listdir(f"{getcwd()}/extracted_image")]
+def save_images(cont, image_subfolder: str):
+    save_subfolder = str(EXTRACTED_IMAGE.joinpath(f"{image_subfolder}"))
+    if not exists(save_subfolder):
+        mkdir(save_subfolder)
+    idx = [int(x[:-4]) for x in listdir(EXTRACTED_IMAGE.joinpath(image_subfolder))]
     number = 0 if len(idx) == 0 else max(idx)
     for c in cont:
         number += 1
         x, y, w, h = cv.boundingRect(c)
         roi = image_to_output[y : y + h, x : x + w]
-        save_subfolder = str(EXTRACTED_IMAGE.joinpath(f"{image_subfolder}"))
-        if not exists(save_subfolder):
-            mkdir(save_subfolder)
         cv.imwrite(f"{save_subfolder}/{str(number)}" + ".jpg", roi)
 
 
@@ -67,11 +70,10 @@ def main(save: bool, image_subfolder: str):
 
 if __name__ == "__main__":
     entry = input("Image name: ")
-    files = listdir(MAIN_DIR)
-    image_entry = next(file for file in files if entry in file.lower())
-    image = str(MAIN_DIR.joinpath(f"{image_entry}"))
+    files = listdir(COLLAGE)
+    image_entry = next(file for file in files if entry.lower() in file.lower())
+    image = str(COLLAGE.joinpath(image_entry))
     print(image)
-    assert image is not None, "image not found"
     cv_img = cv.imread(image)
     img = cv.rotate(cv_img.copy(), cv.ROTATE_90_CLOCKWISE)
     image_to_output = cv.rotate(cv_img, cv.ROTATE_90_CLOCKWISE)
