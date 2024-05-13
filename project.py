0a820cece9ac91454f6e84b3f11ee5679823777a
rfdzan <hekodev.py@gmail.com>
2024-05-13 11:38:46 +0700

    add pip venv, replace os.path with pathlib

--- project.py
+++ project.py
@@ -2,28 +2,25 @@ import cv2 as cv
 from cv2 import UMat
 from pathlib import PurePath, Path
 from typing import Sequence
-from os import mkdir, listdir
-from os.path import exists
 
 MAIN_DIR = PurePath(__file__).parent
 COLLAGE = MAIN_DIR.joinpath("collage")
 EXTRACTED_IMAGE = MAIN_DIR.joinpath("extracted_image")
 
-if not exists(str(EXTRACTED_IMAGE)):
-    mkdir(str(EXTRACTED_IMAGE))
-if not exists(str(COLLAGE)):
-    mkdir(str(COLLAGE))
+if not Path(EXTRACTED_IMAGE).exists():
+    Path(EXTRACTED_IMAGE).mkdir()
+if not Path(COLLAGE).exists():
+    Path(COLLAGE).mkdir()
 
 
 def blur(img) -> UMat:
     img_rgb = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    blur_img = cv.GaussianBlur(img_rgb, tuple(37 for _ in range(2)), 8)
+    blur_img = cv.GaussianBlur(
+        src=img_rgb, ksize=tuple(37 for _ in range(2)), sigmaX=8)
     return blur_img
 
 
-def mask(blur_img) -> UMat:
-    low = 105
-    max_num = 255
+def mask(blur_img, low: int, max_num: int) -> UMat:
     _, thresh = cv.threshold(blur_img, low, max_num, cv.THRESH_BINARY)
     return thresh
 
@@ -48,9 +45,9 @@ def show_boundingrect(cont: Sequence[UMat], cont_image: UMat):
 
 
 def save_images(cont: Sequence[UMat], image_subfolder: str, image_to_output: UMat):
-    save_subfolder = str(EXTRACTED_IMAGE.joinpath(image_subfolder))
-    if not exists(save_subfolder):
-        mkdir(save_subfolder)
+    save_subfolder = EXTRACTED_IMAGE.joinpath(image_subfolder)
+    if not Path(save_subfolder).exists():
+        Path(save_subfolder).mkdir()
     idx = [int(file.stem) for file in Path(save_subfolder).iterdir()]
     number = 0 if len(idx) == 0 else max(idx)
     for c in cont:
@@ -65,7 +62,7 @@ def main(test: bool, image_subfolder: str, image_path: str):
     img = cv.rotate(cv_img.copy(), cv.ROTATE_90_CLOCKWISE)
     image_to_output = cv.rotate(cv_img, cv.ROTATE_90_CLOCKWISE)
     blur_img = blur(img)
-    mask_img = mask(blur_img)
+    mask_img = mask(blur_img, 105, 255)
     cont, cont_img, _ = contour(mask_img, img)
     if not test:
         show_boundingrect(cont, cont_img)
@@ -79,7 +76,7 @@ def cli():
         file for file in files if entry.lower() in file.stem)
     image_path = str(COLLAGE.joinpath(image_entry))
     print(image_path)
-    main(True, image_entry.stem, image_path)
+    main(False, image_entry.stem, image_path)
 
 
 if __name__ == "__main__":
