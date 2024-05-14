6e93952d761d802fe3f44c62d886b1406008264b
rfdzan <hekodev.py@gmail.com>
2024-05-14 16:11:11 +0700

    fixed pytest error, added test case

--- test_project.py
+++ test_project.py
@@ -11,7 +11,7 @@ EXTRACTED_IMAGE = MAIN_DIR.joinpath("extracted_image")
 def open_image() -> tuple[UMat, UMat]:
     image_path = PurePath(COLLAGE).joinpath("test_image.jpg")
     assert Path(image_path).exists()
-    cv_img = cv.imread(image_path)
+    cv_img = cv.imread(str(image_path))
     img = cv.rotate(cv_img.copy(), cv.ROTATE_90_CLOCKWISE)
     image_to_output = cv.rotate(cv_img, cv.ROTATE_90_CLOCKWISE)
     return img, image_to_output
@@ -20,18 +20,17 @@ def open_image() -> tuple[UMat, UMat]:
 def test_blur():
     img, _ = open_image()
     blurred = blur(img)
-    b, g, r = blurred[:, :, 0], blurred[:, :, 1], blurred[:, :, 2]
-    assert (b == g)
-    assert (b == r)
-    ...
+    assert (len(blurred.shape) == 2)
 
 
 def test_mask():
-    ...
-
-
-def test_contour():
-    ...
+    img, image_to_output = open_image()
+    blurred = blur(img)
+    masked = mask(blurred, 105, 255)
+    cont, cont_img, num_of_image = contour(masked, img)
+    # test_image.jpeg only has 4 objects of interest in it
+    # blurring and masking parameters influences the number of images detected
+    assert (num_of_image == 4)
 
 
 def test_save_images():
